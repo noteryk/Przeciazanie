95c5882df080fee1df0abe84f863a6a2d08eb8af
noteryk <74415945+noteryk@users.noreply.github.com>
2024-03-06 20:47:16 +0100

    Add project files.

---
+++ Przeciazanie/Magazyn.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przeciazanie
+{
+    internal class Magazyn
+    {
+        public string Nazwa { get; set; }
+        public List<Produkt> Produkty { get; set; }
+
+        public Magazyn(string nazwa)
+        {
+            Nazwa = nazwa;
+            Produkty = new List<Produkt>();
+        }
+
+        public void DodajProdukt(Produkt produkt)
+        {
+            Produkty.Add(produkt);
+        }
+
+        public void WyświetlProdukty()
+        {
+            foreach (var produkt in Produkty)
+            {
+                Console.WriteLine(produkt);
+            }
+        }
+
+        public void SortujProdukty()
+        {
+            Produkty.Sort((a, b) => a.Cena.CompareTo(b.Cena));
+        }
+    }
+}
---
+++ Przeciazanie/Produkt.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Przeciazanie
+{
+    internal class Produkt
+    {
+        public string Nazwa { get; set; }
+        public decimal Cena { get; set; }
+        public int Ilość { get; set; }
+
+        public Produkt(string nazwa, decimal cena, int ilość)
+        {
+            Nazwa = nazwa;
+            Cena = cena;
+            Ilość = ilość;
+        }
+
+        public static bool operator ==(Produkt a, Produkt b)
+        {
+            return a.Cena == b.Cena;
+        }
+
+        public static bool operator !=(Produkt a, Produkt b)
+        {
+            return !(a == b);
+        }
+
+        public static bool operator <(Produkt a, Produkt b)
+        {
+            return a.Cena < b.Cena;
+        }
+
+        public static bool operator >(Produkt a, Produkt b)
+        {
+            return a.Cena > b.Cena;
+        }
+
+        public static Produkt operator ++(Produkt p)
+        {
+            p.Ilość++;
+            return p;
+        }
+
+        public static Produkt operator --(Produkt p)
+        {
+            p.Ilość--;
+            return p;
+        }
+    }
+
+}
